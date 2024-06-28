0ffb8a21ab01adb229094cadce167345cd54501d
E-Dawkins <ethandawkins@hotmail.co.uk>
2024-06-28 10:42:42 +1000

    Updated About page

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,33 +8,32 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi everyone, I am <span className="purple">Ethan Dawkins </span>
+            from <span className="purple"> Sydney, Australia.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am currently looking for work as a gameplay / game systems engineer.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I have an Advanced Diploma of Professional Game Development from the Academy of Interactive Entertainment.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Playing games
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Learning new technologies
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight /> Walking my dog, Charlie
             </li>
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+            "Don't strive for perfection, strive for results!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Ethan Dawkins</footer>
         </blockquote>
       </Card.Body>
     </Card>
